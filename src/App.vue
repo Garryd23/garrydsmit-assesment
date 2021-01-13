a229d2e451c6bb4edeebf382d3230bd90bb757ce
Garryd <garrydsmit@gmail.com>
2021-01-13 15:31:45 +0200

    Edited loop so numbers range from 1 to the configured number. Converted the string received from the input to a number to avoid type coercion issues. Added a check on the change of the input to eliminate negative numbers and decimals. Also added text selection on focus of the input to avoid having to delete the number before entering a new one. Added styling to numbers to make blocks the size and emphasise hovered number and divisors.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
-    <input type="number" v-model="limit" />
-    <Numbers/>
+    <!-- Set min of input to 0 to prevent arrow controlled decrement from going into negatives --><!-- Set min of input to 0 to prevent arrow controlled decrement from going into negatives -->
+    <!-- Added @focus to select the text when the input is focussed so the user can just type a new number without have to delete the current one first -->
+    <!-- Added v-on:input="cleanNum()" to remove possible negatives or decimals  -->
+    <input
+      type="number"
+      min="0"
+      v-model="limit"
+      v-on:input="cleanNum()"
+      @focus="$event.target.select()"
+    />
+    <Numbers />
   </div>
 </template>
 
 <script>
-import Numbers from '@/components/Numbers';
+import Numbers from "@/components/Numbers";
 
 export default {
-  name: 'App',
-  components: {Numbers},
-  data()
-  {
+  name: "App",
+  components: { Numbers },
+  data() {
     return {
       limit: 100
+    };
+  },
+  //Method cleanNum: added to remove negatives and decimals
+  methods: {
+    cleanNum: function() {
+      var inputNum = this.limit;
+      inputNum = inputNum.replace(/[^\d]/g, "");
+      if (inputNum.indexOf(".") < 0 && inputNum != "") {
+        inputNum = parseInt(inputNum);
+      }
+      this.limit = inputNum;
     }
   }
-}
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,68 +1,86 @@
 <template>
   <div>
-    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset">
-      {{number}}
+    <div
+      class="number"
+      :id="'number-' + number"
+      v-for="number in n()"
+      :key="number"
+      @mouseover="hov(number)"
+      @mouseout="reset"
+    >
+      {{ number }}
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data()
-  {
+  data() {
     return {
       limit: this.$parent.limit,
       numbers: []
-    }
+    };
   },
   watch: {
-    ['$parent.limit'](newLimit)
-    {
+    ["$parent.limit"](newLimit) {
       this.limit = newLimit;
     }
   },
   methods: {
-    n()
-    {
+    n() {
       let numbers = [];
-      for(var i = 0; i < this.limit; i++)
-      {
+      let currentLimit = Number(this.limit); // Converting string to number to avoid type coercion issues
+
+      for (var i = 1; i <= currentLimit; i++) {
+        // 1.There are the configured amount of numbers being displayed but they are ranging from 0. Changed initial value of i=1 so that the numbers start at 1.
+        // 2. Set limit to the new currentLimit variable which is now a number and changed the loop to run until i=currentLimit so that the full range of numbers from 1 to the configured number are displayed. By converting currentLimit to a number it would also of allowed me to set i = currentLimit + 1 to achieve the same result. Previously this would have resulted in 1 being appended to the currentLimit and the output would not be what is required.
         numbers = [...numbers, i];
       }
+      console.log(numbers);
       return numbers.sort(() => Math.random() - 0.5);
     },
-    hov(number)
-    {
-      const nums = document.querySelectorAll('.number');
+    hov(number) {
+      const nums = document.querySelectorAll(".number");
+      console.log(number);
 
-      for(let i = 0; i < nums.length; i++)
-      {
+      for (let i = 0; i < nums.length; i++) {
         const num = nums[i].textContent.trim();
-        if(number % num === 0)
-        {
-          nums[i].classList.add('active')
-          console.log('divisor', num)
+        if (number % num === 0) {
+          nums[i].classList.add("active");
+          console.log("divisor", num);
         }
       }
     },
-    reset()
-    {
-      const nums = document.querySelectorAll('.number');
-      nums.forEach(num => num.classList.remove('active'))
+    reset() {
+      const nums = document.querySelectorAll(".number");
+      nums.forEach(num => num.classList.remove("active"));
     }
   }
-}
+};
 </script>
 
 <style scoped>
-	.number {
-		display: inline-block;
-		padding: 5px;
-		background-color: lightgrey;
-		margin: 5px;
-	}
+.number {
+  display: inline-block;
+  padding: 5px;
+  background-color: lightgrey;
+  /*Added some extra styling for emphasis on hovered number and divisors and to make each number block the same size */
+  width: 30px;
+  height: auto;
+  text-align: center;
+  margin: 10px;
+  cursor: pointer;
+  transition: transform 0.3s ease-out;
+}
 
-	.active {
-		background-color: red;
-	}
+/*Set hovered number's background to green so it is easier to distinguish what number was hovered over.*/
+.number:hover {
+  background-color: green;
+  transform: scale(1.2); /*Emphasise hovered number */
+}
+
+.active {
+  background-color: red;
+  transform: scale(1.2); /*Emphasise the divisor */
+}
 </style>
